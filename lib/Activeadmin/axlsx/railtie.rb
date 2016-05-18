e55c2a75b1b521e5b8e0e0c79b4eee81f2b2eb8b
Armand Niampa <armand.niampa@gmail.com>
2016-05-18 14:15:45 +0200

    Add options in railtie.

--- lib/Activeadmin/axlsx/railtie.rb
+++ lib/Activeadmin/axlsx/railtie.rb
@@ -4,6 +4,8 @@ module Activeadmin
       # This defines configuration items meant to be overriden
       # in the client application.rb file.
       config.activeadmin_axlsx = ActiveSupport::OrderedOptions.new
+      config.activeadmin_axlsx.delete_columns = []
+      config.activeadmin_axlsx.per = 200
     end
   end
 end
